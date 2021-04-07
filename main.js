23544dfe0cb85c3282836a29a89e62ab7787d32e
dmitry <dmitry200@bk.ru>
2021-04-07 10:40:06 +0300

    add basic reg

--- main.js
+++ main.js
@@ -1,84 +1,78 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+const puppeteer = require('puppeteer');
 const Chance = require("chance");
 
 
-var win;
-function createWindow(){
-    win = new BrowserWindow({
-        width: 1500,
-        height: 800,
-        // show: false,
-        center: true,
-        webPreferences: {
-            nodeIntegration: true,
-            devTools: true,
-            preload:`${__dirname}/actionFunc.js`
-          }
+(async () => {
+  const browser = await puppeteer.launch({ 
+        headless: false,
+        // devtools: true,
+        args: [
+            '--proxy-server=127.0.0.1:8888',
+            '--proxy-bypass-list=<-loopback>'
+          ],
+    });
+//   console.log(browser.pages.toString());  
+  const page = await browser.newPage();
+  await page.goto('https://www.instagram.com/accounts/emailsignup/');
+
+  var generator = new Chance(Math.random);
+  var name = generator.name().split(" ");
+  var LastName = name[1];
+  var FirstName = name[0];
+  name = name.join("") + generator.string({length: 7, pool: "abcdertygh1234567890"});
+
+  
+  var email = name +"@outlook.com"
+  var pass = generator.string({length: 10, pool: "qwertyuiopASDfghjklzxcvbnmQWERTYHJUI1234567890"});
+
+
+  await page.waitForTimeout(3000);
+  // await page.waitForNavigation();
+
+   await page.type('input[name="emailOrPhone"]', email, {
+        delay: 2,
+      });
+    await page.type('input[name="fullName"]', LastName, {
+        delay: 2,
+    });
+    await page.type('input[name="username"]', name, {
+        delay: 2,
+    });
+    await page.type('input[name="password"]', pass, {
+        delay: 10,
     });
 
-    win.webContents.openDevTools();
+    (await page.$('button[type="submit"]')).tap();
+    await page.waitForTimeout(3000);
 
-    win.webContents.session.setProxy({
-        // mode: fixed_servers,
-        proxyRules:"localhost:8888" }
-    );
+    // await Promise.all([
+    //   (await page.$('button[type="submit"]')).tap(),
+    //   // page.waitForNavigation(),
+    //   page.waitForTimeout(3000)
+    // ]);
 
-    const url = "https://www.instagram.com/accounts/emailsignup/";
-    loader(win, url);
-}
-
-
-function loader(win, url){
-    var generator = new Chance(Math.random);
-    win.loadURL(url);
-
-
-    var name = generator.name().split(" ");
-    var LastName = name[1];
-    var FirstName = name[0];
-    name = name.join("");
-
-    var email = name + generator.string({length: 7, pool: "abcdertygh1234567890"})+"@outlook.com"
-    var pass = generator.string({length: 10, pool: "qwertyuiopASDfghjklzxcvbnmQWERTYHJUI1234567890"});
-
-    // console.log(name);
-    // console.log(LastName);
-    // console.log(FirstName);
-    // console.log(name);
-    // console.log(email);
-    // console.log(pass);
-
-    win.webContents.on("did-finish-load", () => {
+    var sMonth = generator.integer({ min: 2, max: 11 }).toString();
+    var sYear = generator.integer({ min: 21, max: 30 }).toString();
     
-        // win.webContents.send("replaceMsg", "emailOrPhone", email, 1000);
-        // win.webContents.send("replaceMsg", "fullName", LastName, 2000);
-        // win.webContents.send("replaceMsg", "username", LastName, 3000);
-        // win.webContents.send("replaceMsg", "password", pass, 4000);
-        win.webContents.send("clickObjByText", "Регистрация", 20000);
+    await page.evaluate( () => {
+      document.querySelector('select[title="Месяц:"] > option:nth-child(10)').selected = true;
+      element = document.querySelector('select[title="Месяц:"]');
+      var event = new Event('change', { bubbles: true });
+      event.simulated=true;
+      element.dispatchEvent(event);
     });
 
-}
+    await page.evaluate( () => {
+        // document.querySelector('select[title="Год:"] > option:nth-child('+generator.integer({ min: 21, max: 30 }).toString()+')').selected = true;
+        document.querySelector('select[title="Год:"] > option:nth-child(23)').selected = true;
+        element = document.querySelector('select[title="Год:"]');
+        var event = new Event('change', { bubbles: true });
+        event.simulated=true;
+        element.dispatchEvent(event);
+    });
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
-})
+    await page.waitForTimeout(3000);
+    (await page.$$('button'))[1].tap();
+    // console.log(button);
 
-app.whenReady().then(() => {
-    createWindow();
-
-
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    })
-})
-
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
-})
+})();
